6ab4ad22b88cf2bbd0de546ab4cb5009ce0e6f2b
JONG HYUN <ss961121@naver.com>
2022-10-19 17:56:17 +0900

    10.19 C# SerialPort

---
+++ DeviceSerial.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class DeviceSerial
+    {
+        SerialPort serialPort = new SerialPort();
+        string receiveData = "";
+
+        public bool Connect(int portName, int baudRate = (int)9600, int DateBits = (int)8, Parity parity = Parity.None, StopBits stopBits = StopBits.One)
+        {
+            if (serialPort.IsOpen == false)
+            {
+                serialPort.PortName = "COM" + portName.ToString();
+                serialPort.BaudRate = baudRate;
+                serialPort.Parity = parity;
+                serialPort.StopBits = stopBits;
+                serialPort.DataBits = DateBits;
+              
+                serialPort.Open();
+                serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceiveHandler);
+                serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceiveHandler2);
+            }
+           
+            return true;
+        }
+
+        private void DataReceiveHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort serialPort = (SerialPort) sender;
+            receiveData = serialPort.ReadExisting();
+        }
+
+        public bool Close()
+        {
+            serialPort.Close();
+            
+            return true;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DeviceSerial arduinoPort;
+        public MainWindow()
+        {
+            InitializeComponent();
+            arduinoPort = new DeviceSerial();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            arduinoPort.Connect(3);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            arduinoPort.Close();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
